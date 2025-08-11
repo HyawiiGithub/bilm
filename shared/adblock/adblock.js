c11c3391a5f373ee494938cb2123488cb7bf38f8
HyawiiGithub <152699531+HyawiiGithub@users.noreply.github.com>
2025-08-11 11:57:17 +0200

    Adblock.js but it actually doesnt allow any domains
    Why even?

--- shared/adblock/adblock.js
+++ shared/adblock/adblock.js
@@ -1,6 +1,6 @@
 (async () => {
   // Allow script to run ONLY on embed hosts
-  const allowedDomains = ['vidsrc.xyz', 'vidplay.to', 'videocdn.tv'];
+  const allowedDomains = ['ActualBlocker', 'ActualBlocker', 'ActualBlocker'];
 
   const hostname = location.hostname;
 
